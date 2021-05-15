3a54f9edba9d88493965d660c418ac98519cdc69
Pawel Dardzinski <pawel.dardzinski@wp.pl>
2021-05-15 11:30:07 +0200

    added function (init(),message(),changeBackgroundcolor())

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
-console.log("Cześć Developerzy!");
+{
+    const message = () => {
+        console.log("Cześć Developerzy!");
+    }
 
-let changeBackground = document.querySelector(".changeBackground");
-let body = document.querySelector(".body");
-let colorName = document.querySelector(".colorName");
+    const changeBackgroundcolor = () => {
+        const changeBackground = document.querySelector(".changeBackground");
+        const body = document.querySelector(".body");
+        const colorName = document.querySelector(".colorName");
+        changeBackground.addEventListener("click", () => {
+            body.classList.toggle("backgroundColor2");
+            colorName.innerText = body.classList.contains("backgroundColor2") ? "białe" : "szare";
+        });
+    }
+    
+    const init = () => {
+        changeBackgroundcolor();
+    }
 
-changeBackground.addEventListener("click", () => {
-    body.classList.toggle("backgroundColor2");
-    colorName.innerText = body.classList.contains("backgroundColor2") ? "białe" : "szare";
-});
-
+    message();
+    init();
+}
